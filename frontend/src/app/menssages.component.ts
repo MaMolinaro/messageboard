0c995829f2d7c12fcf6786c1be852a8fc4e7cba6
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2017-12-11 17:10:21 -0200

    Show our data

--- frontend/src/app/menssages.component.ts
+++ frontend/src/app/menssages.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'messages',
-  template: 'This is the Messages Component'
+  template: 'This is the Messages Component <div *ngFor="let message of messages">{{message.text}} by {{message.owner}}</div>'
 })
 export class MessagesComponent {
+    messages = [
+        {
+            text: 'Some text',
+            owner: 'Marco'
+        },
+        {
+            text: 'Other text',
+            owner: 'Sofia'
+        }
+    ];
 }
