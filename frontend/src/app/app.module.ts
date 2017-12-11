7720d3f47b235a18972083bbf9aecc49251c8c93
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2017-12-11 18:29:10 -0200

    Refining your layout Angular with Material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { 
+      MatButtonModule, 
+      MatCardModule,
+      MatInputModule,
+      MatSnackBarModule,
+      MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MessagesComponent } from './menssages.component';
@@ -11,7 +17,13 @@ import { MessagesComponent } from './menssages.component';
     AppComponent, MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatCardModule, 
+    MatInputModule,
+    MatSnackBarModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
