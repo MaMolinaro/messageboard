7ee4cf249f567a1bda63b115cbee1d9899f9d87e
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2017-12-15 18:00:20 -0200

    Handle errors with try or catch

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -2,6 +2,7 @@ import { Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class WebService {
@@ -10,17 +11,30 @@ export class WebService {
 
     messages = [];
 
-    constructor(private http: Http) {
+    constructor(private http: Http, private sb: MatSnackBar) {
         this.getMessages();
     }
 
     async getMessages() {
-        var response = await this.http.get(this.BASE_URL + '/messages').toPromise();
-        this.messages = response.json();
+        try {
+            var response = await this.http.get(this.BASE_URL + '/messages').toPromise();
+            this.messages = response.json();          
+        } catch (error) {
+            this.handleError("Unable to get messages!");
+        }
     }
 
     async postMessage(message) {
-        var response = await this.http.post(this.BASE_URL + '/messages', message).toPromise();
-        this.messages.push(response.json());
+        try {
+            var response = await this.http.post(this.BASE_URL + '/messages', message).toPromise();
+            this.messages.push(response.json());                
+        } catch (error) {
+            this.handleError("Unable to post message!");
+        }
+    }
+
+    private handleError(error) {
+        console.log(error);
+        this.sb.open(error, 'close', {duration: 3000});
     }
 }
