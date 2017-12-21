708ac552b7484e3ece43a41af709215bfb55be81
Marco Aurelio Molinaro <molinaromarcoaurelio@gmail.com>
2017-12-20 22:48:05 -0200

    Validate email with a custom validator

--- frontend/src/app/register.component.ts
+++ frontend/src/app/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -15,10 +15,10 @@ export class RegisterComponent {
         this.form = fb.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
-            email: ['', Validators.required],
+            email: ['', [Validators.required, emailValid()]],
             password: ['', Validators.required],
             confirmPassword: ['', Validators.required]
-        }, { validators: matchingFields('password', 'confirmPassword')})
+        }, { Validator: matchingFields('password', 'confirmPassword')})
     }
 
     onSubmit() {
@@ -33,7 +33,16 @@ export class RegisterComponent {
 
 function matchingFields(field1, field2) {
     return form => {
-        if (form.constrols[field1].value !== form.constrols[field2].value) 
-            return { mismatchedFields: true }
+        if (form.constrols[field1].value !== form.constrols[field2].value) {
+            return { 'mismatchedFields': true }
+        }
+    }
+}
+
+function emailValid() {
+    return control => {
+        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        return regex.test(control.value) ? null : {invalidEmail: true}
     }
 }
